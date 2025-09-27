ebe2c583e3ba572f51118c8ace5e8c3064fab419
Muhammad Wajeeh <muhammadwajeeh04@gmail.com>
2025-09-26 20:03:35 -0500

    Added Profile Management Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,31 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Login from './Pages/Login'; 
-import Register from './Pages/Register';
+import "./App.css";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import ProfileManagement from "./Pages/ProfileManagement";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
     <>
-      
-      
       <Router>
-      <Routes>
-        <Route
-          path="/Login"
-          element = {<Login></Login>}
-            />
-            <Route
-          path="/Register"
-          element = {<Register></Register>}
-            />
-          
-          
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/Login" element={<Login></Login>} />
+          <Route path="/Register" element={<Register></Register>} />
+          <Route
+            path="/ProfileManagement"
+            element={<ProfileManagement></ProfileManagement>}
+          />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
