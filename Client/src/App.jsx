38a0f324e17d00c0cfbc817c187dfd427ff93e9b
Muhammad Wajeeh <muhammadwajeeh04@gmail.com>
2025-09-26 22:36:07 -0500

    Added sidebar to base route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,7 @@ import ProfileManagement from "./Pages/ProfileManagement";
 import EventManagement from "./Pages/EventManagement";
 import VolunteerHistory from "./Pages/VolunteerHistory";
 import VolunteerMatching from "./Pages/VolunteerMatching";
+import Sidebar from "./Pages/Sidebar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,6 +15,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Sidebar></Sidebar>} />
           <Route path="/Login" element={<Login></Login>} />
           <Route path="/Register" element={<Register></Register>} />
           <Route
