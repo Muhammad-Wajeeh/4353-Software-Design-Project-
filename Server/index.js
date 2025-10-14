e089d3ab2e7b0b7267c548a35fc47d7643000faf
Muhammad Wajeeh <muhammadwajeeh04@gmail.com>
2025-10-13 23:40:50 -0500

    Connected Frontend to Backend Endpoint

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
+const bcrypt = require("bcrypt");
+
+//middleware
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.post("/register", async (req, res) => {
+  const { firstName, lastName, username, password, email } = req.body;
+  // console.log(req.body);
+  try {
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    await pool.query(
+      "INSERT INTO members (firstName, LastName, username, password, email) VALUES($1, $2, $3, $4, $5)",
+      [firstName, lastName, username, hash, email]
+    );
+    res.json("Account Created");
+  } catch (err) {
+    console.log(err.message);
+    // need something here to let client side know that the user they are attempting is taken, or the id, maybe even enforce stronger password
+  }
+});
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
