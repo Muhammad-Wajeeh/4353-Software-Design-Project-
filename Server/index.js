3718dc4915fe7f1a8a741b91a3f9fe8038612f29
Muhammad Wajeeh <muhammadwajeeh04@gmail.com>
2025-10-15 21:45:16 -0500

    Split Index into differnt route files

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 // const pool = require("./db");
 const bcrypt = require("bcrypt");
+const authRoutes = require("./routes/authRoutes");
 
 //middleware
 
@@ -15,37 +16,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/register", async (req, res) => {
-  const { firstName, lastName, username, email, password} = req.body;
-  console.log(req.body);
-
-  try {
-    // hard coding the storage for now
-    const tempStorageForRegistrationInfo = {
-      firstName: firstName,
-      lastName: lastName,
-      username: username,
-      password: password,
-      email: email,
-    };
-
-    console.log(tempStorageForRegistrationInfo)
-
-    // ----------- will be used when we implement DB
-
-    // const saltRounds = 10;
-    // const hash = await bcrypt.hash(password, saltRounds);
-
-    // await pool.query(
-    //   "INSERT INTO members (firstName, LastName, username, password, email) VALUES($1, $2, $3, $4, $5)",
-    //   [firstName, lastName, username, hash, email]
-    // );
-    res.json("Account Created");
-  } catch (err) {
-    console.log(err.message);
-    // need something here to let client side know that the user they are attempting is taken, or the id, maybe even enforce stronger password
-  }
-});
+app.use("/", authRoutes);
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
---
+++ Server/routes/authRoutes.js
@@ -0,0 +1,46 @@
+// routes/userRoutes.js
+const express = require("express");
+const router = express.Router();
+
+// Define routes specific to users
+router.get("/", (req, res) => {
+  res.send("Get all users");
+});
+
+router.post("/", (req, res) => {
+  res.send("Create a new user");
+});
+
+router.get("/register", async (req, res) => {
+  const { firstName, lastName, username, email, password } = req.body;
+  console.log(req.body);
+
+  try {
+    // hard coding the storage for now
+    const tempStorageForRegistrationInfo = {
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      password: password,
+      email: email,
+    };
+
+    console.log(tempStorageForRegistrationInfo);
+
+    // ----------- will be used when we implement DB
+
+    // const saltRounds = 10;
+    // const hash = await bcrypt.hash(password, saltRounds);
+
+    // await pool.query(
+    //   "INSERT INTO members (firstName, LastName, username, password, email) VALUES($1, $2, $3, $4, $5)",
+    //   [firstName, lastName, username, hash, email]
+    // );
+    res.json("Account Created");
+  } catch (err) {
+    console.log(err.message);
+    // need something here to let client side know that the user they are attempting is taken, or the id, maybe even enforce stronger password
+  }
+});
+
+module.exports = router;
